933f548f927563d2c19705af33fb2cbd98056a9a
Mugunthan N <100777814+mugunthan17@users.noreply.github.com>
2025-02-14 12:22:28 +0530

    Update vite.config.js
    updated vite config file for servo motor proxy

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      "/open-gate": {
+        //target: 'http://192.168.1.40',
+        target: "http://192.168.33.124",
+        changeOrigin: true,
+      },
+    },
+  },
 })
