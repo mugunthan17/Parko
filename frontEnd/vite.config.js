9c3e3292efd6d8edeb61dc951f38da5f85d221f5
Mugunthan N <100777814+mugunthan17@users.noreply.github.com>
2025-02-14 17:58:48 +0530

    Update vite.config.js
    update vite.config.js with ngrok link

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
       "/open-gate": {
         //target: 'http://192.168.1.40',
         //target: "http://192.168.33.124",
-        target: "http://18.142.128.26",
+        //target: "http://18.142.128.26",
+        target: "https://7b45-2409-40f4-3-c33d-c113-5c36-751-fc5a.ngrok-free.app/",
         changeOrigin: true,
       },
     },
