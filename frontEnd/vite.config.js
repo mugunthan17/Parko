42543dce16797b5f03b9b092a3aeaaa79a1b78a4
Mugunthan N <100777814+mugunthan17@users.noreply.github.com>
2025-02-26 13:21:11 +0530

    Update vite.config.js

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         //target: 'http://192.168.1.40',
         //target: "http://192.168.33.124",
         //target: "http://18.142.128.26",
-        target: "https://7b45-2409-40f4-3-c33d-c113-5c36-751-fc5a.ngrok-free.app/",
+        target: "https://78cc-2409-40f4-40ce-54b7-a01e-1e30-c25-f34b.ngrok-free.app",
         changeOrigin: true,
       },
     },
