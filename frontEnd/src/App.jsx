dc1031ec7fc7e91ee4a26df8d5502d1206eb1571
Sarweshwaran R S <110753390+sarweshwaran-rs@users.noreply.github.com>
2025-03-26 15:09:15 +0530

    Update_App.jsx_NGROK

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -72,7 +72,7 @@ function App() {
       alert("Your Payment is Successful! 🎉 Gate opening...");
 
       // Redirect to ngrok link
-      const ngrokLink = "https://9a4f-2409-40f4-ad-a316-ccfb-fcda-46f0-8c23.ngrok-free.app/open_gate?status=success&charging=" + formData.chargingFacility.toLowerCase();
+      const ngrokLink = "https://a784-2409-40f4-a9-b0ea-997c-70a0-6644-1ce5.ngrok-free.app/open_gate?status=success&charging=" + formData.chargingFacility.toLowerCase();
       window.location.href = ngrokLink;
 
       // After 1 second, redirect to the final page
